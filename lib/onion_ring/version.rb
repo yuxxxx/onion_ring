b6e86ef8285e17d3528dd519467e64d23958432c
ゆうなんとか <yuunnxx@gmail.com>
2015-05-29 23:36:23 +0900

    Gem作ってみた

---
+++ lib/onion_ring/version.rb
@@ -0,0 +1,3 @@
+module OnionRing
+  VERSION = "0.1.0"
+end
--- sample/sample.rb
+++
@@ -1,6 +0,0 @@
-require '../onion_ring.rb'
-border = OnionRing::run('before.png', 'after.png')
-
-# borderのleft, top, right, bottomのピクセル数が帰ってくる
-p "border: #{border}"
-p 'success!'
